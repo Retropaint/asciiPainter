108be02ca011e3cab18f17df36b8db010ba467aa
Osman Mammedov <darkglasses1122@gmail.com>
2024-09-06 22:40:50 +0800

    remove selection anchor

--- main.cpp
+++ main.cpp
@@ -390,6 +390,7 @@ void getInput() {
 			edit(selection[i].ascii, pos.x, pos.y, false, true, true);
 			edit(selection[i].color, pos.x, pos.y, true, true, true);
 		}
+		selection.clear();
 	}
 
 	if(k == input[FLOODFILL]) tryFloodFill(cursor.x, cursor.y, (char)k, colorMode);
